9448a8683ed623ca74f9b49f66f1a14b0ce25cca
backspace229 <backspace229@gmail.com>
2015-11-17 13:01:17 +0900

    Objectの種類分けに関するもの
    Atom, ChemicalBond, FunctionGroupなど

---
+++ Assets/ChemicalObjects.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChemicalObjects : MonoBehaviour {
+
+    string type; // "Chemical Bond", "Atom", "function group"
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    string getType()
+    {
+        return type;
+    }
+}
